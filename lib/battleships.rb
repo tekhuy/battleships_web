387e21137d6086b8ddbe691b562a26b2089231f8
tekhuy <huyle20@hotmail.com>
2015-01-07 17:37:58 +0000

    updated start.erb

--- lib/battleships.rb
+++ lib/battleships.rb
@@ -1,15 +1,15 @@
 require 'sinatra'
 
 class BattleShips < Sinatra::Base
+
+  enable :sessions
   
   get '/' do
     erb :index
   end
 
   get '/new' do
-    # erb :name
-    # @name = params[:name
-     erb :name
+    erb :name
   end
 
   post '/new' do
@@ -24,7 +24,25 @@ class BattleShips < Sinatra::Base
   end
 
   get '/start' do
+    # session[:counter] = 0
+    # @counter = session[:counter]
+    session[:ship_coord] = []
     erb :start 
+  end
+
+  post '/start' do
+    # session[:counter] += 1
+    # @counter = session[:counter]
+
+    if params[:coordinate].empty?
+      @error_coord = "You didn't enter a co-ordinate"
+      erb :start
+    else
+      @coordinate = params[:coordinate]
+      session[:ship_coord] << @coordinate
+      @ship_coord = session[:ship_coord]
+      erb :start
+    end
   end
 
   get '/try_again' do
